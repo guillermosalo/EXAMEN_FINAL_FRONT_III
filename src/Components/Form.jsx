5ee8797fccc20b9b685dbcf2e963826477b923b8
guill <guillermosalo@gmail.com>
2022-12-09 05:14:37 -0300

    UseInputHook y Form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { useState } from "react";
+import useInputContact from "./utils/UseInputContact";
 
 
 const Form = () => {
   //Aqui deberan implementar el form completo con sus validaciones
-  const [name, setName] = useState('')
+
+    const name = useInputContact('text')
+    const lastName = useInputContact('text')
+    const email = useInputContact('email')
+    const consulta = useInputContact('text')
+    const submit = useInputContact('submit')
 
   return (
     <div>
       <form ation=''>
-        <input type="text" value= {name} onChange={(e) => setName(e.target.value)} />
-        <input type="email" />
+        <input {...name}/>
+        <input {...lastName}/>
+        <input {...email}/>
+        <input {...consulta}/>
       </form>
     </div>
   );
